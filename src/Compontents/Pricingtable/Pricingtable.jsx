1215d269b993d551be60d77141b9d67d69bc7313
Nabil Islam Hamja <hamjanabil11@gmail.com>
2025-05-17 14:24:50 +0600

    pricing table update

--- src/Compontents/Pricingtable/Pricingtable.jsx
+++ src/Compontents/Pricingtable/Pricingtable.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Container from '../layout/Container'
 import user1 from '../../assets/user1.png'
 import { FiCheck } from "react-icons/fi";
+import user2 from '../../assets/user2.png'
+import user3 from '../../assets/user3.png'
 
 function Pricingtable() {
     return (
@@ -39,7 +41,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Custom Templates</p>
                                 </div>
 
 
@@ -47,7 +49,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>list view</p>
                                 </div>
 
 
@@ -57,7 +59,7 @@ function Pricingtable() {
                                     </div>
 
 
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>board view</p>
                                 </div>
 
 
@@ -65,7 +67,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Assignees & Due Dates</p>
                                 </div>
 
 
@@ -80,13 +82,13 @@ function Pricingtable() {
                     <div className="w-[450px] h-[698px] border  border-[#DCE7FE]  rounded-[20px] group hover:bg-secondary transition-all duration-500   ">
                         <div className=" pt-[40px] text-center">
                             <div className="flex justify-center m-auto  items-center w-[90px] h-[90px] rounded-full bg-[#E8F2FA] group-hover:bg-white transition-all duration-500 ">
-                                <img src={user1} alt="" />
+                                <img src={user2} alt="" />
                             </div>
 
                             <div className="text-center  ">
-                                <p className='font-paimary  text-[24px] font-bold  leading-[36px]  text-secondary mt-[32px] mb-[2px] group-hover:text-[#43E7DF] transition-all duration-500  '>Personal</p>
-                                <h2 className='font-paimary    font-bold text-[44px] leading-[54px] text-tittle group-hover:text-white transition-all duration-500 '>$10/mth</h2>
-                                <p className='font-secondary m-auto text-[18px] font-medium leading-[18px] mt-[14px] mb-[40px] text-[#645E76] w-[320px]  group-hover:text-white transition-all duration-500  '>Annual pricing <span className='text-secondary'>(save 20%)</span> </p>
+                                <p className='font-paimary  text-[24px] font-bold  leading-[36px]  text-secondary mt-[32px] mb-[2px] group-hover:text-[#43E7DF] transition-all duration-500  '>Team</p>
+                                <h2 className='font-paimary    font-bold text-[44px] leading-[54px] text-tittle group-hover:text-white transition-all duration-500 '>$25/mth</h2>
+                                <p className='font-secondary m-auto text-[18px] font-medium leading-[18px] mt-[14px] mb-[40px] text-[#645E76] w-[320px]  group-hover:text-white transition-all duration-500  '>Annual pricing (save 35%) <span className='text-secondary'>(save 20%)</span> </p>
                             </div>
 
                             <div className="my-[40px] px-[60px]  ">
@@ -95,7 +97,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Timeline Review</p>
                                 </div>
 
 
@@ -103,7 +105,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Custom fields</p>
                                 </div>
 
 
@@ -111,7 +113,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Custom Templates</p>
                                 </div>
 
 
@@ -121,7 +123,7 @@ function Pricingtable() {
                                     </div>
 
 
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Task dependencies</p>
                                 </div>
 
 
@@ -129,7 +131,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Milestones Manage</p>
                                 </div>
 
 
@@ -144,13 +146,13 @@ function Pricingtable() {
                     <div className="w-[450px] h-[698px] border  border-[#DCE7FE]  rounded-[20px] group hover:bg-secondary transition-all duration-500   ">
                         <div className=" pt-[40px] text-center">
                             <div className="flex justify-center m-auto  items-center w-[90px] h-[90px] rounded-full bg-[#E8F2FA] group-hover:bg-white transition-all duration-500 ">
-                                <img src={user1} alt="" />
+                                <img src={user3} alt="" />
                             </div>
 
                             <div className="text-center  ">
-                                <p className='font-paimary  text-[24px] font-bold  leading-[36px]  text-secondary mt-[32px] mb-[2px] group-hover:text-[#43E7DF] transition-all duration-500  '>Personal</p>
-                                <h2 className='font-paimary    font-bold text-[44px] leading-[54px] text-tittle group-hover:text-white transition-all duration-500 '>$10/mth</h2>
-                                <p className='font-secondary m-auto text-[18px] font-medium leading-[18px] mt-[14px] mb-[40px] text-[#645E76] w-[320px]  group-hover:text-white transition-all duration-500  '>Annual pricing <span className='text-secondary'>(save 20%)</span> </p>
+                                <p className='font-paimary  text-[24px] font-bold  leading-[36px]  text-secondary mt-[32px] mb-[2px] group-hover:text-[#43E7DF] transition-all duration-500  '>Enterprise</p>
+                                <h2 className='font-paimary    font-bold text-[44px] leading-[54px] text-tittle group-hover:text-white transition-all duration-500 '>$50/mth</h2>
+                                <p className='font-secondary m-auto text-[18px] font-medium leading-[18px] mt-[14px] mb-[40px] text-[#645E76] w-[320px]  group-hover:text-white transition-all duration-500  '>Annual pricing (save 40%) <span className='text-secondary'>(save 20%)</span> </p>
                             </div>
 
                             <div className="my-[40px] px-[60px]  ">
@@ -159,7 +161,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Data export & deletion</p>
                                 </div>
 
 
@@ -167,7 +169,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Block native integrations</p>
                                 </div>
 
 
@@ -175,7 +177,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Custom branding</p>
                                 </div>
 
 
@@ -185,7 +187,7 @@ function Pricingtable() {
                                     </div>
 
 
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Lock custom fields</p>
                                 </div>
 
 
@@ -193,7 +195,7 @@ function Pricingtable() {
                                     <div className="flex justify-center  items-center w-[24px] h-[24px] rounded-full bg-[#E8F2FA]  group-hover:bg-white transition-all duration-500  ">
                                         <FiCheck className='' />
                                     </div>
-                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Calendar View</p>
+                                    <p className='font-secondary ml-[22px]  text-[18px] font-medium  leading-[32px]  text-details  group-hover:text-white transition-all duration-500 '>Priority support</p>
                                 </div>
 
 
