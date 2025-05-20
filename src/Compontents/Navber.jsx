3a2a2adc3474cb839fd5dcf3f9b67ae81a263e62
Nabil Islam Hamja <hamjanabil11@gmail.com>
2025-05-20 12:56:29 +0600

    navbar issue solve

--- src/Compontents/Navber.jsx
+++ src/Compontents/Navber.jsx
@@ -14,18 +14,18 @@ const Navber = () => {
        </div>
 
 
-      <div className="  "><ul className='flex gap-[50px] font-secondary  font-medium text-lg  text-white'>
-        <li> <a href="">home</a></li>
-        <li><a href="">Features</a> </li>
-        <li><a href="">Service</a></li>
-        <li><a href="">Pages</a></li>
-        <li> <a href="">Blog</a></li>
+      <div className="  "><ul className='flex gap-[50px] font-secondary  font-medium text-lg  text-white '>
+        <li> <a  className=' group hover:text-red-500 transition-all duration-500' href="">home</a></li>
+        <li><a  className='group hover:text-red-500 transition-all duration-500 'href="">Features</a> </li>
+        <li><a  className='group hover:text-red-500 transition-all duration-500 'href="">Service</a></li>
+        <li><a className='group hover:text-red-500 transition-all duration-500 ' href="">Pages</a></li>
+        <li> <a className='group hover:text-red-500 transition-all duration-500 ' href="">Blog</a></li>
       </ul>
       </div>
 
 
       <div className=" flex gap-[36px]  items-center">
-        <p className='font-secondary font-medium text-lg  cursor-pointer text-white'>Login</p>
+        <p className='font-secondary font-medium text-lg  cursor-pointer text-white group hover:text-red-500 transition-all duration-500 '>Login</p>
         <a className=' font-secondary cursor-pointer font-medium text-lg py-5 px-12 bg-paimary rounded-[6px] text-secondary'>Register</a>
      </div>
       </div>
